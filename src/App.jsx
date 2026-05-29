d8af614c6852883e90f7085545ec193afc42d2c8
Sri Satya <bhamidipatisreesatya8@gmail.com>
2026-05-29 09:40:32 +0530

    Integrated homepage section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
 import { useState } from 'react'
 import Preloader from './components/Preloader'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import TrustBar from './components/TrustBar'
-import About from './components/About'
-import Services from './components/Services'
-import Initiatives from './components/Initiatives'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
+import HomeScreen from './HomeScreen'
 import useScrollReveal from './hooks/useScrollReveal'
 
 export default function App() {
@@ -23,16 +16,7 @@ export default function App() {
           loading ? 'pointer-events-none h-screen overflow-hidden opacity-0' : 'opacity-100'
         }`}
       >
-        <Navbar />
-        <main>
-          <Hero />
-          <TrustBar />
-          <About />
-          <Services />
-          <Initiatives />
-          <Contact />
-        </main>
-        <Footer />
+        {!loading && <HomeScreen />}
       </div>
     </>
   )
